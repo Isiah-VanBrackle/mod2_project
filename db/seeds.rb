ff4afccb2133ac2a989a406822f6c7e6063501bf
me <juniorjr083@gmail.com>
2018-08-22 11:06:47 -0400

    commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Book.create(title:"It's lit", description: "SO and so and so and so more lorem Ipsom", author: "Mike Knoll", img_url: "/images/book-1.jpg")
+Book.create(title:"When It rains it pours", description: "SO and so and so and so more lorem Ipsom", author: "Betty Ross", img_url: "/images/book-2.jpg")
+Book.create(title:"The Constitution", description: "SO and so and so and so more lorem Ipsom", author: "Tupac Shakur", img_url: "/images/book-3.jpg")
+Book.create(title:"It's lit", description: "SO and so and so and so more lorem Ipsom", author: "Francis Key", img_url: "/images/book-4.jpg")
+Book.create(title:"It's lit", description: "SO and so and so and so more lorem Ipsom", author: "Tammy Rogers", img_url: "/images/book-5.jpg")
